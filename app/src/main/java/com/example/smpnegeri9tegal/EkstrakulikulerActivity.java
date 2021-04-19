dba728981ad501d843118617ca4ac0c6b05ad60d
mhmmdilhamnhs23 <muhammadilhamnurhasani>
2021-04-19 22:52:51 +0700

    v6

--- app/src/main/java/com/example/smpnegeri9tegal/EkstrakulikulerActivity.java
+++ app/src/main/java/com/example/smpnegeri9tegal/EkstrakulikulerActivity.java
@@ -13,42 +13,4 @@ public class EkstrakulikulerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ekstrakulikuler);
     }
 
-    public void hadroh(View view){
-        Intent intent=new Intent(EkstrakulikulerActivity.this, HadrohActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void sepakbola(View view){
-        Intent intent=new Intent(EkstrakulikulerActivity.this, SepakbolaActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void karate(View view){
-        Intent intent=new Intent(EkstrakulikulerActivity.this, KarateActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void silat(View view){
-        Intent intent=new Intent(EkstrakulikulerActivity.this, SilatActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void pmr(View view){
-        Intent intent=new Intent(EkstrakulikulerActivity.this, PmrActivity.class);
-        startActivity(intent);
-
-    }
-
-    public void pramuka(View view){
-        Intent intent=new Intent(EkstrakulikulerActivity.this, PramukaActivity.class);
-        startActivity(intent);
-
-    }
-
-
-
 }
